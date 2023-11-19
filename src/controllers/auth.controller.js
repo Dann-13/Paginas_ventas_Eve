7c1a15eab741253cf516308a633014d0d04d6c00
Dann-13 <dannyelgomezz.13@gmail.com>
2023-11-19 01:37:56 -0500

    Resolviendo error conexion

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,41 +1,48 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs'
 import { createAccessToken } from '../libs/jwt.js'
-
 export const register = async (req, res) => {
-    const { email, password, username } = req.body;
+    const { username, email, password } = req.body;
+
     try {
-        const passwordHash = await bcrypt.hash(password, 10);//encriptammos contraseña
+        const passwordHash = await bcrypt.hash(password, 10);
         const newUser = new User({
             username,
             email,
             passwordHash
         });
-        const userSaved = await newUser.save();
-        const token = await createAccessToken({ id: userSaved._id });
 
-        res.cookie("token", token);
-        res.json({
-            id: userSaved.id,
-            username: userSaved.username,
-            email: userSaved.email
-        });
+        try {
+            const userSaved = await newUser.save();
+            const token = await createAccessToken({ id: userSaved._id });
+
+            res.cookie("token", token);
+            res.json({
+                id: userSaved.id,
+                username: userSaved.username,
+                email: userSaved.email
+            });
+        } catch (saveError) {
+            console.error("Error saving user:", saveError);
+            res.status(500).json({ message: "Error saving user" });
+        }
     } catch (err) {
-        res.status(500).json({message:err.message});
+        console.error("Error hashing password:", err);
+        res.status(500).json({ message: err.message });
     }
 };
-
 export const login = async (req, res) => {
     const { email, password } = req.body;
     try {
-        const userFound = await User.findOne({email});
-        if(!userFound) return res.status(400).json({message: "Usuario no encontrados"});
+        const userFound = await User.findOne({ email });
+        if (!userFound) return res.status(400).json({ message: "Usuario no encontrados" });
 
         const isMatch = await bcrypt.compare(password, userFound.passwordHash);//encriptammos contraseña
-        if(!isMatch) return res.status(400).json({message: "Contraseña incorrecta"});
-     
+        if (!isMatch) return res.status(400).json({ message: "Contraseña incorrecta" });
 
-        const token = await createAccessToken({ id: userFound._id, admin: userFound.isAdmin
+
+        const token = await createAccessToken({
+            id: userFound._id, admin: userFound.isAdmin
         });
 
         res.cookie("token", token);
@@ -45,11 +52,11 @@ export const login = async (req, res) => {
             email: userFound.email
         });
     } catch (err) {
-        res.status(500).json({message:err.message});
+        res.status(500).json({ message: err.message });
     }
 };
 
-export const logout = (req, res) =>{
+export const logout = (req, res) => {
     res.cookie('token', "", {
         expires: new Date(0),
         httpOnly: true
@@ -58,7 +65,7 @@ export const logout = (req, res) =>{
 }
 export const profile = async (req, res) => {
     const userFound = await User.findById(req.user.id);
-    if(!userFound) return res.status(400).json({message:"user not found"});
+    if (!userFound) return res.status(400).json({ message: "user not found" });
     return res.json({
         id: userFound.id,
         username: userFound.username,
